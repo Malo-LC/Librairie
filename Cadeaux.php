97a0fc9fa6b33510b5e11ec286f889186b542b30
malo <malolc2001@gmail.com>
2021-05-23 18:53:02 +0200

    Barre de recherche

--- Cadeaux.php
+++ Cadeaux.php
@@ -2,18 +2,58 @@
 session_start();
 
 ?>
+
+<?php 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=librairie;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM livres');
+$donnees = $reponse->fetch();
+$reponse1 = $bdd->query('SELECT * FROM livres');
+$donnees1 = $reponse1->fetch();
+
+$i=1;
+$nb_id=0;
+do{
+	$nb_id++;
+} while($donnees1 = $reponse1->fetch());
+
+
+
+$livre = array_fill(1,$nb_id,array_fill(1,5,0));
+do{
+	if($donnees['ID']==$i){
+		$livre[$i][1]=$i;
+		$livre[$i][2]=$donnees['titre'];
+		$livre[$i][3]=$donnees['auteur'];
+		$livre[$i][4]=$donnees['photo'];
+		$livre[$i][5]=$donnees['synopsis'];
+	}
+	$i++;
+}
+while($donnees = $reponse->fetch());
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8" />
-    <title>Librairie</title>
+    <title>Librairie - Recherche</title>
     <link rel="stylesheet" type="text/css" href="stylelibrairie.css" />
     <link rel="icon" type="image/png" sizes="16x16" href="icone.png">
+    <LINK rel="stylesheet" type="text/css" href="barre_recherche.css" media="screen" />
 
 </head>
 
 
 <body>
+
+
 
 	<?php include("entete.php"); ?>
 
@@ -26,39 +66,75 @@ session_start();
 		<li class="toolbar left active" style="float:right"><a href="Cadeaux.php">À propos</a></li>
     </ul>
 		
-	
-    
-
-
-
-
-	
-
-	
-
-
 	<div id="vitrine">
 		<div class="presentoir">
-		<div class="couverure"><img src="livre.jpg" alt="Livre" class="livretab"></div>
-		<p>9.99€</p>	<img class="ajout_panier" alt="Panier" src="cart.png">
+		<div class="couverure"><a href="livre<?php echo $id1;?>.php"><img src="<?php $ID=1; echo $livre[$ID][4];?>" alt="Livre" class="livretab"></a></div>
+		<div class="flex"><p><?php echo $livre[$livre[$ID][1]][3];?></p><p><?php echo $livre[$livre[$ID][1]][2]; ?></p></div>
 		</div>
 		<div class="presentoir">
-		<div class="couverure"><img src="livre.jpg" alt="Livre" class="livretab"></div>
-		<p>9.99€</p>	<img class="ajout_panier" alt="Panier" src="cart.png">
+		<div class="couverure"><a href="livre<?php echo $id1;?>.php"><img src="<?php $ID++; echo $livre[$ID][4];?>" alt="Livre" class="livretab"></a></div>
+		<div class="flex"><p><?php echo $livre[$livre[$ID][1]][3];?></p><p><?php echo $livre[$livre[$ID][1]][2]; ?></p></div>
 		</div>
-				<div class="presentoir">
-		<div class="couverure"><img src="livre.jpg" alt="Livre" class="livretab"></div>
-		<p>9.99€</p>	<img class="ajout_panier" alt="Panier" src="cart.png">
-		</div>
-				<div class="presentoir">
-		<div class="couverure"><img src="livre.jpg" alt="Livre" class="livretab"></div>
-		<p>9.99€</p>	<img class="ajout_panier" alt="Panier" src="cart.png">
-		</div>
-				<div class="presentoir">
-		<div class="couverure"><img src="livre.jpg" alt="Livre" class="livretab"></div>
-		<p>9.99€</p>	<img class="ajout_panier" alt="Panier" src="cart.png">
+		<div class="presentoir">
+		<div class="couverure"><a href="livre<?php echo $id1;?>.php"><img src="<?php $ID++; echo $livre[$ID][4];?>" alt="Livre" class="livretab"></a></div>
+		<div class="flex"><p><?php echo $livre[$livre[$ID][1]][3];?></p><p><?php echo $livre[$livre[$ID][1]][2]; ?></p></div>
 		</div>
 	</div>
+    
+ 
+    <input id="searchbar" onkeyup="search_animal()" type="text"
+        name="search" placeholder="Chercher un livre..">
+      
+    
+    <ol id='list'>
+        <li class="livres">Cat</li>
+        <li class="livres">Dog</li>
+    </ol>
+      
+    <script type="text/javascript">
+    	<?php $ID=1; ?>
+    	function ajouter(){
+    		var liste =document.getElementById("list");
+    		var titre = document.createElement('li');
+    		var lien = document.createElement('a');
+    		var txt = '<?php echo $livre[$ID][2]; ?>';
+    		var lientxt = 'livre'+'<?php echo $livre[$ID][1]; $ID++; ?>'+'.php';
+    		lien.href=lientxt;
+    		titre.setAttribute( 'class', 'livres' );
+    		lien.textContent= txt;
+    		titre.appendChild(lien);
+    		liste.appendChild(titre);
+    		
+
+
+    	}
+    	var n = '<?php echo $nb_id; ?>';
+    	for( i = 0 ; i < n ; i ++){
+    		ajouter();
+
+    	}
+    	
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('livres');
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+    </script>
+
+
+
+	
+
+	
+<br><br>
 
 <div id="conteneur">
 	<p> A PROPOS DE LA LIBRAIRIE </p>
