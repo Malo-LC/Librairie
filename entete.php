eea80dd58217532884b3a4a108d929431ae397bd
malo <malolc2001@gmail.com>
2021-05-26 13:51:01 +0200

    imprimer curseur

--- entete.php
+++ entete.php
@@ -5,7 +5,7 @@
 			<div class="liens"> <a href="Presse.php"> <img class="lien" alt="Panier" src="cart.png">  </a> <p>Panier</p></div>
 			<div class="liens"> <a href="connexion.php"> <img class="lien" alt="Compte" src="compte.png"> </a><p>Compte</p> </div>
 			<div class="liens"> <a href="contact.php"> <img class="lien" alt="Contact" src="contact.png"> </a> <p>Contact</p></div>
-			<div class="liens"> <a onclick="window.print()"> <img class="lien" alt="Imprimer" src="print.png"> </a> <p>Imprimer</p></div>
+			<div class="liens" style="cursor: pointer;"> <a onclick="window.print()"> <img class="lien" alt="Imprimer" src="print.png"> </a> <p>Imprimer</p></div>
 
 
 		</div>
